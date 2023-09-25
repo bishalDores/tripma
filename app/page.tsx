23648dfe1d7d12c214d9b6d59c7070124e3900e2
bishal <bishalpd@gmail.com>
2023-09-25 21:54:10 +0600

    card update and few sections added

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <FlightStatus />
         </div>
       </div>
-      <div className="container mx-auto mt-10">
+      <div className="container mx-auto mt-10 px-4 sm:px-0">
         <div className="flex items-center justify-between mb-6 lg:flex-row flex-col">
           <h2 className="text-[24px] text-[#6E7491] font-bold">
             Find your next adventure with these{" "}
@@ -28,10 +28,89 @@ export default function Home() {
             </Link>
           </h2>
         </div>
-        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-10 mb-10">
-          <Card img="/images/sanghai.jpg" />
-          <Card img="/images/sydney.jpg" />
-          <Card img="/images/kyoto.jpg" />
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-10 mb-10 justify-items-center md:justify-items-start">
+          <Card
+            img="/images/sanghai.jpg"
+            description="China’s most international city"
+            price="$598"
+            title="The Bund, Shanghai"
+            titleColor="text-primary-default"
+          />
+          <Card
+            img="/images/sydney.jpg"
+            description="Take a stroll along the famous harbor"
+            price="$981"
+            title="Sydney Opera House, Sydney"
+            titleColor="text-primary-default"
+          />
+          <Card
+            img="/images/kyoto.jpg"
+            description="Step back in time in the Gion district"
+            price="$633"
+            title="Kōdaiji Temple, Kyoto"
+            titleColor="text-primary-default"
+          />
+        </div>
+        <div className="w-full h-[480px] mt-10 mb-20 rounded-xl card bg-white">
+          <div className="w-full h-[397px] relative rounded-xl ">
+            <Image src="/images/showcase.jpg" layout="fill" alt="blog image" />
+          </div>
+          <div className="px-6 py-4 flex justify-between items-center">
+            <div>
+              <h4 className="text-[#6E7491] font-semibold">
+                Tsavo East National Park, <span className="text-primary-default">Kenya</span>
+              </h4>
+              <p className="text-[14px] text-primary-inactive">
+                Named after the Tsavo River, and opened in April 1984, Tsavo East National Park is one of the oldest parks in Kenya. It is located in
+                the semi-arid Taru Desert.
+              </p>
+            </div>
+            <div className="text-[#6E7491] font-semibold">$1,248</div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mb-6 lg:flex-row flex-col">
+          <h2 className="text-[24px] text-[#6E7491] font-bold">
+            Explore unique{" "}
+            <span className="text-[#5CD6C0]">
+              <Link href={"/"}>places to stay</Link>
+            </span>
+          </h2>
+
+          <h2 className="text-[24px] text-[#6E7491]">
+            <Link href={"/"} className="flex items-center">
+              All
+              <Image src={"/images/arrowRight.svg"} width={32} height={33} alt="arrow right" />
+            </Link>
+          </h2>
+        </div>
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-10 mb-10 justify-items-center md:justify-items-start">
+          <Card
+            img="/images/maldives.jpg"
+            description="From the 2nd century AD, the islands were known as the 'Money Isles' due to the abundance of cowry shells, a currency of the early ages."
+            price={undefined}
+            title="Stay among the atolls in Maldives"
+            titleColor="text-[#5CD6C0]"
+          />
+          <Card
+            img="/images/moroco.jpg"
+            description="Morocco’s Hispano-Moorish architecture blends influences from Berber culture, Spain, and contemporary artistic currents in the Middle East."
+            price={undefined}
+            title="Experience the Ourika Valley in Morocco"
+            titleColor="text-[#5CD6C0]"
+          />
+          <Card
+            img="/images/mongolia.jpg"
+            description="Traditional Mongolian yurts consists of an angled latticework of wood or bamboo for walls, ribs, and a wheel."
+            price={undefined}
+            title="Live traditionally in Mongolia"
+            titleColor="text-[#5CD6C0]"
+          />
+        </div>
+        <div className="text-center my-10">
+          <Link href={"/"} className="py-3 px-5 bg-primary-default text-white rounded text-[14px]">
+            Explore more stays
+          </Link>
         </div>
       </div>
     </main>
--- components/inHouse/Card.tsx
+++ components/inHouse/Card.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import React from "react";
 
-const Card = ({ img }: { img: string }) => {
+const Card = ({
+  img,
+  description,
+  price,
+  title,
+  titleColor,
+}: {
+  img: string;
+  description: string;
+  price: string | undefined;
+  title: string;
+  titleColor: string;
+}) => {
   return (
-    <div className="sm:w-[411px] w-[300px] h-[397px] rounded-xl card bg-white">
+    <div className="sm:w-[411px] w-[300px] min-h-[397px] rounded-xl card bg-white">
       <div className="sm:w-[411px] w-[300px] h-[320px] relative rounded-xl ">
         <Image src={img} layout="fill" alt="blog image" />
       </div>
@@ -11,11 +23,11 @@ const Card = ({ img }: { img: string }) => {
       <div className="px-6 py-4 flex justify-between items-center">
         <div>
           <h4 className="text-[#6E7491] font-semibold">
-            The Bund, <span className="text-primary-default">Shanghai</span>
+            {title.substring(0, title.lastIndexOf(" "))} <span className={titleColor}>{title.split(" ")[title.split(" ").length - 1]}</span>
           </h4>
-          <p className="text-[14px] text-primary-inactive">China’s most international city</p>
+          <p className="text-[14px] text-primary-inactive">{description}</p>
         </div>
-        <div className="text-[#6E7491] font-semibold">$598</div>
+        {price && <div className="text-[#6E7491] font-semibold">{price}</div>}
       </div>
     </div>
   );
